bbf2274d56451ce681579bfb978d3437ae8e2ae5
夏风飞舞 <1217491317@qq.com>
2019-05-17 00:11:31 +0800

    Update

---
+++ pages/edit/edit.js
@@ -0,0 +1,42 @@
+
+Page({
+
+  data: {
+    cdata: [
+      {
+        no: 1,
+        type: 'txt',
+        txt: 'woshiwenben1'
+      },
+      {
+        no: 2,
+        type: 'txt',
+        txt: 'woshiwenben2'
+      },
+      {
+        no: 3,
+        type: 'img',
+        img: '/images/index-head.png'
+      },
+      {
+        no: 4,
+        type: 'video',
+        video: 'http://vjs.zencdn.net/v/oceans.mp4'
+      },
+    ]
+  },
+
+  onLoad: function (options) {
+
+  },
+
+
+  onReady: function () {
+
+  },
+
+  onShow: function () {
+
+  },
+
+})
